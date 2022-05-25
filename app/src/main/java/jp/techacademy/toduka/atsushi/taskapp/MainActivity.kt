40d9b4cfcd8ffc92d464d7524734ec88e56ae717
atsushi.toduka <sysutemu09@gmail.com>
2022-05-25 19:40:54 +0900

    TaskAppやり直し

--- app/src/main/java/jp/techacademy/toduka/atsushi/taskapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/toduka/atsushi/taskapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package jp.techacademy.toduka.atsushi.taskapp
+
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 import io.realm.RealmChangeListener
 import io.realm.Sort
-import java.util.*
+import android.content.Intent
+import androidx.appcompat.app.AlertDialog
+import android.app.AlarmManager
+import android.app.PendingIntent
+const val EXTRA_TASK = "jp.techacademy.toduka.atsushi.taskapp.TASK"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mRealm: Realm
@@ -23,8 +27,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
         }
 
         // Realmの設定
@@ -35,18 +39,51 @@ class MainActivity : AppCompatActivity() {
         mTaskAdapter = TaskAdapter(this)
 
         // ListViewをタップしたときの処理
-        listView1.setOnItemClickListener { parent, view, position, id ->
+        listView1.setOnItemClickListener { parent, _, position, _ ->
             // 入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
         }
 
         // ListViewを長押ししたときの処理
-        listView1.setOnItemLongClickListener { parent, view, position, id ->
+        listView1.setOnItemLongClickListener { parent, _, position, _ ->
             // タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            // ダイアログを表示する
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+
+            builder.setPositiveButton("OK"){_, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+                val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+                val resultPendingIntent = PendingIntent.getBroadcast(
+                    this,
+                    task.id,
+                    resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+                alarmManager.cancel(resultPendingIntent)
+                reloadListView()
+            }
+
+            builder.setNegativeButton("CANCEL", null)
+
+            val dialog = builder.create()
+            dialog.show()
+
             true
         }
-
-        // アプリ起動時に表示テスト用のタスクを作成する
-        addTaskForTest()
 
         reloadListView()
     }
@@ -69,16 +106,5 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
 
         mRealm.close()
-    }
-
-    private fun addTaskForTest() {
-        val task = Task()
-        task.title = "作業"
-        task.contents = "プログラムを書いてPUSHする"
-        task.date = Date()
-        task.id = 0
-        mRealm.beginTransaction()
-        mRealm.copyToRealmOrUpdate(task)
-        mRealm.commitTransaction()
     }
 }
--- app/src/main/java/jp/techacademy/toduka/atsushi/taskapp/Task.kt
+++ app/src/main/java/jp/techacademy/toduka/atsushi/taskapp/Task.kt
@@ -1,11 +1,16 @@
 package jp.techacademy.toduka.atsushi.taskapp
 
-import android.app.Application
-import io.realm.Realm
+import java.io.Serializable
+import java.util.Date
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
-class TaskApp: Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Realm.init(this)
-    }
+open class Task : RealmObject(), Serializable {
+    var title: String = ""      // タイトル
+    var contents: String = ""   // 内容
+    var date: Date = Date()     // 日時
+
+    // idをプライマリーキーとして設定
+    @PrimaryKey
+    var id: Int = 0
 }
